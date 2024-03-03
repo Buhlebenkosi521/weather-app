5aa161f76aeb14dcd6f2cf3d97a94988dddbcbf6
Buhlebenkosi521 <157530679+Buhlebenkosi521@users.noreply.github.com>
2024-03-03 20:36:00 +0200

    updated the search input and button,and also changed the color in button.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("buhle");
+function handleSearchSubmit(event){
+    event.preventDefault();
+    let searchInput =document.querySelector("#searchInput");
+    let button =document.querySelector("#button");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML= `${searchInput.value}`;
+}
+button.addEventListener("click", handleSearchSubmit);
