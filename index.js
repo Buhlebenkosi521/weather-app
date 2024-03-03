1e332ec737174c837f5236a0204c0105098dad3c
Buhlebenkosi521 <157530679+Buhlebenkosi521@users.noreply.github.com>
2024-03-03 21:04:47 +0200

    added apiKey and Url

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function searchCity(city) {
+    let apiKey = "cec85bc06c0dfd484345cb4a3249819e";
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric";
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput =document.querySelector("#searchInput");
