a9553dd03632656e0e1d979d06a407add572ad52
Buhlebenkosi521 <157530679+Buhlebenkosi521@users.noreply.github.com>
2024-03-03 21:50:41 +0200

    polished my css and added another function.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+function refreshWeather(response) {
+    let cityElement=document.querySelector("#city");
+    let temperatureElement = document.querySelector("#temperature");
+
+    temperatureElement.innerHTML = response.data.main.temp;
+    cityElement.innerHTML = response.data.name;
+}
+
+
 function searchCity(city) {
     let apiKey = "cec85bc06c0dfd484345cb4a3249819e";
-    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric";
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric";
     axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -9,7 +18,7 @@ function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput =document.querySelector("#searchInput");
     let button =document.querySelector("#button");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML= `${searchInput.value}`;
 }
 button.addEventListener("click", handleSearchSubmit);
+
+refreshWeather();
