cb252c93c818e503a953b73b5552a18d68405590
Buhlebenkosi521 <157530679+Buhlebenkosi521@users.noreply.github.com>
2024-03-05 05:51:39 +0200

    i have undeclared cityelement from handleSubmit function and activated temperature and city response.

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 function refreshWeather(response) {
+    console.log(response.data.main.temp);
     let cityElement = document.querySelector("#city");
     let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.main.temp;
 
-    temperatureElement.innerHTML = response.data.main.temp;
+
     cityElement.innerHTML = response.data.name;
+    temperatureElement.innerHTML = Math.round(response.data.main.temp) + "°c";
 }
-
 
 function searchCity(city) {
     let apiKey = "cec85bc06c0dfd484345cb4a3249819e";
-    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
 }
-
 
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput =document.querySelector("#searchInput");
-let cityElement=document.querySelector("#city");
-cityElement.innerHTML= `${searchInput.value}`;
     let button =document.querySelector("#button");
-
+searchCity(searchInput.value);
 }
 button.addEventListener("click", handleSearchSubmit);
 
-refreshWeather();
+searchCity();
