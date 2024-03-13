b9cc37cf0dd751828140514ccd5b5e153c42df20
Buhlebenkosi521 <157530679+Buhlebenkosi521@users.noreply.github.com>
2024-03-13 11:29:33 +0200

    i polished my code, i added the forest and styled it.

--- index.js
+++ index.js
@@ -1,61 +1,68 @@
 function refreshWeather(response) {
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.main.temp;
+  let humidity = document.querySelector(".humidity");
+  let speed = document.querySelector(".speed");
+  let description = document.querySelector("#description");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.dt * 1000);
+  let iconCode = response.data.weather[0].icon;
+  let iconElement = document.querySelector("#icon");
 
-    let cityElement = document.querySelector("#city");
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.main.temp;
-    let humidity = document.querySelector(".humidity");
-    let speed = document.querySelector(".speed");
-    let description = document.querySelector("#description");
-let timeElement = document.querySelector("#time");
-let date = new Date(response.data.dt*1000);
-let iconCode = response.data.weather[0].icon;
-let iconElement = document.querySelector("#icon");
-
-    cityElement.innerHTML = response.data.name;
-    temperatureElement.innerHTML = Math.round(response.data.main.temp) + "°c";
-humidity.innerHTML = response.data.main.humidity + "%";
-speed.innerHTML = Math.round(response.data.wind.speed) + "km/h";
-description.innerHTML = response.data.weather[0].description;
-time.innerHTML = formatDate(date);
-icon.innerHTML = `<img src=https://openweathermap.org/img/wn/${iconCode}@2x.png class="weatherIcon"/>`;
-
-
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp) + "°c";
+  humidity.innerHTML = response.data.main.humidity + "%";
+  speed.innerHTML = Math.round(response.data.wind.speed) + "km/h";
+  description.innerHTML = response.data.weather[0].description;
+  time.innerHTML = formatDate(date);
+  icon.innerHTML = `<img src=https://openweathermap.org/img/wn/${iconCode}@2x.png class="weatherIcon"/>`;
 }
 
-function formatDate(date){
-    
-    let hours = date.getHours();
-    let minutes =date.getMinutes();
-    let days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday","Saturday",];
-    let day = days[date.getDay()];
-    if (minutes < 10){
-        minutes = `0${minutes}`;
-    }
-return `${day} ${hours}:${minutes}`;
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
-    let apiKey = "cec85bc06c0dfd484345cb4a3249819e";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(refreshWeather);
+  let apiKey = "cec85bc06c0dfd484345cb4a3249819e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput =document.querySelector("#searchInput");
-    let button =document.querySelector("#button");
-searchCity(searchInput.value);
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#searchInput");
+  let button = document.querySelector("#button");
+  searchCity(searchInput.value);
+}
+function getForecast() {
+  let apiKey = "cec85bc06c0dfd484345cb4a3249819e";
 }
 
-function displayForecast(){
-let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
-let forecastHTML = "";
-days.forEach(function (day) {
-forecastHTML =
-forecastHTML +    
- `
- 
-<div class="weather-forecast-day">
+function displayForecast() {
+  let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = "";
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+<div class="weather-forecast" id="forecast">
+<div class="weather-forecast-item">
 <div class="weather-forecast-date">${day}</div>
 <div class="weather-forecast-icon">🌧️</div>
 <div class="weather-forecast-temperatures">
@@ -64,10 +71,9 @@ forecastHTML +
 </div>
 </div>
 `;
-
-});
-let forecast = document.querySelector("#forecast");
-forecast.innerHTML = forecastHTML;
+  });
+  let forecast = document.querySelector("#forecast");
+  forecast.innerHTML = forecastHTML;
 }
 button.addEventListener("click", handleSearchSubmit);
 
